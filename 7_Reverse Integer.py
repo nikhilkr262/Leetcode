b6ef3930519f2f208c6762117842f1d447eefdba
nikhilkr262 <103442017+nikhilkr262@users.noreply.github.com>
2022-07-10 18:46:13 +0530

    Day 6

---
+++ 7_Reverse Integer.py
@@ -0,0 +1,18 @@
+class Solution:
+    def reverse(self, x: int) -> int:
+        isNegative = False
+        if x < 0:
+            isNegative = True
+            x = -x
+        rev=0
+        while x:
+            rev=rev*10+x%10
+            x=x//10
+        if rev >= ((2 ** 31) - 1) or rev <= (-2 ** 31):
+            return 0
+        if isNegative==True:
+            return -rev
+        else:
+            return rev
+            
+        
